4218a3783285bdf2fa29e44420071bf536d11da0
JulioAlonsoCesarDR <jac.dr1013@gmail.com>
2023-04-24 01:39:30 -0600

    clean code

--- src/components/products/CardProduct.jsx
+++ src/components/products/CardProduct.jsx
@@ -10,11 +10,10 @@ import { useState } from "react";
 import { formatMoney } from "../../helpers/helper";
 import useContextProvider from "../../hooks/useContextProvider.jsx";
 
+const CardProduct = ({ product }) => {
+  const { addProduct } = useContextProvider();
 
-const CardProduct = ({product}) => {
-    const {addProduct} = useContextProvider()
-
-    const [isHover, setIsHover] = useState(false);
+  const [isHover, setIsHover] = useState(false);
 
   const ListStars = [];
   let stars = 0;
@@ -40,7 +39,7 @@ const CardProduct = ({product}) => {
         onMouseEnter={() => setIsHover(true)}
         onMouseLeave={() => setIsHover(false)}
       >
-        <CardActionArea onClick={() => addProduct()} >
+        <CardActionArea onClick={() => addProduct()}>
           <CardMedia
             component="img"
             height="140"
@@ -49,9 +48,6 @@ const CardProduct = ({product}) => {
           />
           <CardContent className="d-flex flex-column align-items-center">
             <span>{product.productName}</span>
-            <Typography gutterBottom variant="" component="div">
-              titulo
-            </Typography>
             <div className="d-flex flex-row">{ListStars} </div>
             <span>
               de {formatMoney(product.listPrice) ?? formatMoney(product.price)}
@@ -59,12 +55,17 @@ const CardProduct = ({product}) => {
             <b>Por ${formatMoney(product.price)}</b>
             <span>
               {product.installments.length >= 0
-                ? `o en ${product.installments[0].quantity}x de R ${formatMoney(product.installments[0].value)}`
+                ? `o en ${product.installments[0].quantity}x de R ${formatMoney(
+                    product.installments[0].value
+                  )}`
                 : ""}
             </span>
-            <div className="py-3" style={{height:'50px'}}>
+            <div className="py-3" style={{ height: "50px" }}>
               {isHover && (
-                <button className="btn btn-dark buttonStyle" onClick={() => addProduct()}>
+                <button
+                  className="btn btn-dark buttonStyle"
+                  onClick={() => addProduct()}
+                >
                   Comprar
                 </button>
               )}
